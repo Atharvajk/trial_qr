45d4ddd408841a4d78182d5c654653eb3ff6dab4
akash69-bit <haraleakash3@gmail.com>
2023-05-12 21:58:05 +0530

    second_commit_by_akash

--- firstcpp.cpp
+++ firstcpp.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
 using namespace std;
-
+void function()
+{
+    cout<<"Im a new function";
+}
 int main(){
     cout<<"Helloworld to Atharva";
     cout<<"heyyy";
+    function();
     return 0;
 }
